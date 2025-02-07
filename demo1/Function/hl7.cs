9006b20fc2efe1e75a2f472172e153a52d28cc70
Nathan Fernandez <nathan@interprit.com>
2025-02-07 13:21:39 +1100

    added .NET library

--- demo1/Function/hl7.cs
+++ demo1/Function/hl7.cs
@@ -10,6 +10,7 @@ namespace interprit
     using Microsoft.Azure.Functions.Extensions.Workflows;
     using Microsoft.Azure.Functions.Worker;
     using Microsoft.Extensions.Logging;
+    using Interprit.Demo.HL7;
 
     /// <summary>
     /// Represents the hl7 flow invoked function.
@@ -29,52 +30,17 @@ namespace interprit
         /// <param name="zipCode">The zip code.</param>
         /// <param name="temperatureScale">The temperature scale (e.g., Celsius or Fahrenheit).</param>
         [Function("hl7")]
-        public Task<Weather> Run([WorkflowActionTrigger] int zipCode, string temperatureScale)
+        public Task<ADT_A01> Run([WorkflowActionTrigger] string HL7Message)
         {
-            this.logger.LogInformation("Starting hl7 with Zip Code: " + zipCode + " and Scale: " + temperatureScale);
-            
-            // Generate random temperature within a range based on the temperature scale
-            Random rnd = new Random();
-            var currentTemp = temperatureScale == "Celsius" ? rnd.Next(1, 30) : rnd.Next(40, 90);
-            var lowTemp = currentTemp - 10;
-            var highTemp = currentTemp + 10;
+            this.logger.LogInformation("Starting hl7");
 
-            // Create a Weather object with the temperature information
-            var weather = new Weather()
-            {
-                ZipCode = zipCode,
-                CurrentWeather = $"The current weather is {currentTemp} {temperatureScale}",
-                DayLow = $"The low for the day is {lowTemp} {temperatureScale}",
-                DayHigh = $"The high for the day is {highTemp} {temperatureScale}"
-            };
 
-            return Task.FromResult(weather);
+            ADT_A01 adt01 = new ADT_A01(HL7Message);
+
+
+
+            return Task.FromResult(adt01);
         }
 
-        /// <summary>
-        /// Represents the weather information for hl7.
-        /// </summary>
-        public class Weather
-        {
-            /// <summary>
-            /// Gets or sets the zip code.
-            /// </summary>
-            public int ZipCode { get; set; }
-
-            /// <summary>
-            /// Gets or sets the current weather.
-            /// </summary>
-            public string CurrentWeather { get; set; }
-
-            /// <summary>
-            /// Gets or sets the low temperature for the day.
-            /// </summary>
-            public string DayLow { get; set; }
-
-            /// <summary>
-            /// Gets or sets the high temperature for the day.
-            /// </summary>
-            public string DayHigh { get; set; }
-        }
     }
 }
---
+++ demo1/Interprit.Demo.Hl7/ADT_01.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interprit.Demo.HL7
+{
+    /// <summary>
+    /// An A01 event is sent as a result of a patient undergoing the admission process which assigns the patient to a bed. 
+    /// It signals the beginning of a patient's stay in a healthcare facility.
+    /// </summary>
+    public class ADT_A01
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ADT_A01"/> class.
+        /// </summary>
+        public ADT_A01()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ADT_A01"/> class.
+        /// </summary>
+        /// <param name="hl7String">The HL7 string.</param>
+        public ADT_A01(string hl7String)
+        {
+        }
+    }
+}
+
+        
